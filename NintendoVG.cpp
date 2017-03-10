6113a11b9779562871771bf29cd8a05d9eb1e792
Jose Rojas <jose23rojas@unitec.edu>
2017-03-09 23:37:41 -0600

    clase NintendoVG, hija de VideoJuego TERMINADA

---
+++ NintendoVG.cpp
@@ -0,0 +1,10 @@
+// NintendoVG.cpp - hija de VideoJuego
+
+#include "NintendoVG.h"
+
+NintendoVG::NintendoVG()
+{
+}
+NintendoVG::NintendoVG(string nombre, string year, Consola* consola, int numJugadores, string genero, bool estado, string numSerie, double precio) : VideoJuego(nombre, year, consola, numJugadores, genero, estado, numSerie, precio)
+{
+}
---
+++ NintendoVG.h
@@ -0,0 +1,41 @@
+// NintendoVG.h - hija de VideoJuego
+
+#include "VideoJuego.h"
+
+#ifndef NINTENDOVG_H
+#define NINTENDOVG_H
+
+class NintendoVG : public VideoJuego
+{
+        public:
+                NintendoVG();
+                NintendoVG(string, string, Consola*, int, string, bool, string, double);
+                // nombre, year, Consola*, numJugadores, genero, estado, numSerie, precio
+
+                // nombre
+                virtual void setNombre(string);
+                virtual string getNombre();
+                // year
+                virtual void setYear(string);
+                virtual string getYear();
+                // Consola*
+                virtual void setConsola(Consola*);
+                virtual Consola* getConsola();
+                // numJugadores
+                virtual void setNumJugadores(int);
+                virtual int getNumJugadores();
+                // genero
+                virtual void setGenero(int);
+                virtual string getGenero();
+                // estado
+                virtual void setEstado(bool);
+                virtual string getEstado();
+                // numSerie
+                virtual void setNumSerie(string);
+                virtual string getNumSerie();
+                // precio
+                virtual void setPrecio(double);
+                virtual double getPrecio();
+};
+
+#endif
